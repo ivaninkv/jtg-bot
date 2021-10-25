4bb97bf9e68534456f55e4d2423bd95ac1e0cc85
Konstantin Ivanin <ivanin.k@gmail.com>
2021-10-25 18:19:34 +0300

    Change init script for add rows to city table

--- mysql-init/001_Create-two-tables.sql
+++ mysql-init/001_Create-two-tables.sql
@@ -2,19 +2,28 @@ CREATE DATABASE cities;
 USE cities;
 
 CREATE TABLE country (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30),
     population INT );
 
 CREATE TABLE city (
-    id INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30),
     country_id INT,
     population INT,
     FOREIGN KEY (country_id)
         REFERENCES country(id) );
 
-INSERT INTO country (id, name, population) VALUES (1, 'Ukraine', 41806221);
-INSERT INTO country (id, name, population) VALUES (2, 'Russia', 146171015);
-INSERT INTO country (id, name, population) VALUES (3, 'Belorus', 9349645);
-INSERT INTO country (id, name, population) VALUES (4, 'Moldova', 2640438);
+INSERT INTO country (id, name, population) VALUES
+    (1, 'Ukraine', 41806221),
+    (2, 'Russia', 146171015),
+    (3, 'Belorus', 9349645),
+    (4, 'Moldova', 2640438);
+
+INSERT INTO city (name, country_id, population) VALUES
+    ('Kharkov', 1, 1443000),
+    ('Kyiv', 1, 3703100),
+    ('Minsk', 3, 2545500),
+    ('Odessa', 1, 1017699),
+    ('Voronezh', 2, 1058261),
+    ('Kishinev', 4, 695400);
