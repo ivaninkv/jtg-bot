6c64af018bd3371dae941de64dc836278df6e8e2
Konstantin Ivanin <ivanin.k@gmail.com>
2021-10-30 19:22:27 +0300

    Create Spring project

---
+++ src/main/resources/db/migration/V00001_Create_Chat_Table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE chat (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+);
---
+++ src/test/java/com/github/ivaninkv/jtgbot/JavaTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ivaninkv.jtgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
