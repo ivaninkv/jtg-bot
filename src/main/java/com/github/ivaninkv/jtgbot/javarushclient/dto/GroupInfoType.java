251b07b94158099ef4a63e26b011bcca86c78e4e
Konstantin Ivanin <ivanin.k@gmail.com>
2021-11-13 15:34:20 +0300

    JTG-7: add javarush api group client

---
+++ src/main/java/com/github/ivaninkv/jtgbot/javarushclient/dto/GroupInfoType.java
@@ -0,0 +1,5 @@
+package com.github.ivaninkv.jtgbot.javarushclient.dto;
+
+public enum GroupInfoType {
+    UNKNOWN, CITY, COMPANY, COLLEGE, TECH, SPECIAL, COUNTRY
+}
---
+++ src/main/java/com/github/ivaninkv/jtgbot/javarushclient/dto/GroupVisibilitysdtatus.java
@@ -0,0 +1,5 @@
+package com.github.ivaninkv.jtgbot.javarushclient.dto;
+
+public enum GroupVisibilitysdtatus {
+    UNKNOWN, RESTRICTED, PUBLIC, PROTECTED, PRIVATE, DISABLED, DELETED
+}
